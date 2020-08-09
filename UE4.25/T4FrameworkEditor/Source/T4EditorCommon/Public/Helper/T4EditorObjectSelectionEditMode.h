244b00ef262d8d03d365ef047ab7f111cdb3a567
SoonBo Noh <niceofer@hotmail.com>
2020-08-09 19:09:22 +0900

    [v0.9.9] Deploying T4Framework Plugins - #155 [버그수정] Content Editor, Edit Mode 에서 WidgetMode 변경 후 편집이 안되던 문제 수정

--- UE4.25/T4FrameworkEditor/Source/T4EditorCommon/Public/Helper/T4EditorObjectSelectionEditMode.h
+++ UE4.25/T4FrameworkEditor/Source/T4EditorCommon/Public/Helper/T4EditorObjectSelectionEditMode.h
@@ -25,20 +25,22 @@ public:
 	static const FEditorModeID EM_T4EditorObjectSelectionEditMode;
 
 	/** FEdMode interface */
-	virtual bool StartTracking(FEditorViewportClient* InViewportClient, FViewport* InViewport) override;
-	virtual bool EndTracking(FEditorViewportClient* InViewportClient, FViewport* InViewport) override;
-	virtual bool InputDelta(FEditorViewportClient* InViewportClient, FViewport* InViewport, FVector& InDrag, FRotator& InRot, FVector& InScale) override;
-	virtual void Render(const FSceneView* View, FViewport* Viewport, FPrimitiveDrawInterface* PDI) override;
-	virtual void DrawHUD(FEditorViewportClient* ViewportClient, FViewport* Viewport, const FSceneView* View, FCanvas* Canvas) override;
-	virtual bool AllowWidgetMove() override;
-	virtual bool ShouldDrawWidget() const override;
-	virtual bool UsesTransformWidget() const override;
-	virtual bool UsesTransformWidget(FWidget::EWidgetMode CheckMode) const override;
-	virtual FVector GetWidgetLocation() const override;
-	virtual bool GetCustomDrawingCoordinateSystem(FMatrix& InMatrix, void* InData) override;
-	virtual bool GetCustomInputCoordinateSystem(FMatrix& InMatrix, void* InData) override;
-	virtual bool HandleClick(FEditorViewportClient* InViewportClient, HHitProxy *HitProxy, const FViewportClick &Click) override;
-	virtual bool CanCycleWidgetMode() const override;
+	void Enter() override;
+	void Exit() override;
+	bool StartTracking(FEditorViewportClient* InViewportClient, FViewport* InViewport) override;
+	bool EndTracking(FEditorViewportClient* InViewportClient, FViewport* InViewport) override;
+	bool InputDelta(FEditorViewportClient* InViewportClient, FViewport* InViewport, FVector& InDrag, FRotator& InRot, FVector& InScale) override;
+	void Render(const FSceneView* View, FViewport* Viewport, FPrimitiveDrawInterface* PDI) override;
+	void DrawHUD(FEditorViewportClient* ViewportClient, FViewport* Viewport, const FSceneView* View, FCanvas* Canvas) override;
+	bool AllowWidgetMove() override;
+	bool ShouldDrawWidget() const override;
+	bool UsesTransformWidget() const override;
+	bool UsesTransformWidget(FWidget::EWidgetMode CheckMode) const override;
+	FVector GetWidgetLocation() const override;
+	bool GetCustomDrawingCoordinateSystem(FMatrix& InMatrix, void* InData) override;
+	bool GetCustomInputCoordinateSystem(FMatrix& InMatrix, void* InData) override;
+	bool HandleClick(FEditorViewportClient* InViewportClient, HHitProxy *HitProxy, const FViewportClick &Click) override;
+	bool CanCycleWidgetMode() const override;
 
 public:
 	void SetViewModel(IT4EditorViewModel* InViewModel) { ViewModelRef = InViewModel; } // #94
--- UE4.25/T4FrameworkEditor/Source/T4EditorCommon/Public/T4EditorCommonConstants.h
+++ UE4.25/T4FrameworkEditor/Source/T4EditorCommon/Public/T4EditorCommonConstants.h
@@ -8,7 +8,7 @@
   * #39, #90
  */
 
-static const TCHAR* T4Editor_EditorVersionName = TEXT("v0.9.8");
+static const TCHAR* T4Editor_EditorVersionName = TEXT("v0.9.9");
 
 static const FName T4Editor_ConstnatTableMapZone = TEXT("MapZone"); // #92 : Int Value
 static const FName T4Editor_ConstnatTableTimeTag = TEXT("TimeTag"); // #90 : Float Value
